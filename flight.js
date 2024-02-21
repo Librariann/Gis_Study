1a6da6e1f392fb68375057d2228e7b9c04480690
Librariann <okpc0306@naver.com>
2024-02-21 10:00:10 +0900

    add flight tracker

--- flight.js
+++ flight.js
@@ -11,7 +11,7 @@ const viewer = new Cesium.Viewer("cesiumContainer", {
 const osmBuildings = viewer.scene.primitives.add(Cesium.createOsmBuildings());
 
 const getJson = async () => {
-  return await fetch("./test.json").then((res) => res.json());
+  return await fetch("./path.json").then((res) => res.json());
 };
 
 const times = [];
@@ -36,7 +36,7 @@ const flight = async () => {
     targetCoordinate.height
   );
 
-  const timeStepInSeconds = 30;
+  const timeStepInSeconds = 20;
   const totalSeconds = timeStepInSeconds * (flightData.length - 1);
   const start = Cesium.JulianDate.fromDate(new Date(2019, 5, 10, 13));
   const stop = Cesium.JulianDate.addSeconds(
@@ -53,10 +53,10 @@ const flight = async () => {
   // Start playing the scene.geatigoonore
   viewer.clock.shouldAnimate = false;
 
-  flightData.forEach((point, index) => {
+  flightData.forEach((point, i) => {
     const time = Cesium.JulianDate.addSeconds(
       startTime,
-      index * 10,
+      i * timeStepInSeconds,
       new Cesium.JulianDate()
     );
     const position = Cesium.Cartesian3.fromDegrees(
@@ -106,12 +106,6 @@ const flight = async () => {
     return property;
   };
 
-  const computeOrientation = () => {
-    const property = new Cesium.TimeIntervalCollectionProperty();
-
-    return property;
-  };
-
   // 방위각 계산 함수
   function calculateCorrectBearing(startLat, startLon, endLat, endLon) {
     const startLatRad = Cesium.Math.toRadians(startLat);
@@ -133,55 +127,6 @@ const flight = async () => {
     return bearingDegrees;
   }
 
-  // 방위각 계산 함수
-  function calculateBearingUsingCesium(startLat, startLon, endLat, endLon) {
-    const startCartographic = Cesium.Cartographic.fromDegrees(
-      startLon,
-      startLat
-    );
-    const endCartographic = Cesium.Cartographic.fromDegrees(endLon, endLat);
-
-    const startCartesian = Cesium.Cartesian3.fromRadians(
-      startCartographic.longitude,
-      startCartographic.latitude,
-      0
-    );
-    const endCartesian = Cesium.Cartesian3.fromRadians(
-      endCartographic.longitude,
-      endCartographic.latitude,
-      0
-    );
-
-    const vector = Cesium.Cartesian3.subtract(
-      endCartesian,
-      startCartesian,
-      new Cesium.Cartesian3()
-    );
-    Cesium.Cartesian3.normalize(vector, vector);
-
-    const eastVector = Cesium.Cartesian3.cross(
-      Cesium.Cartesian3.UNIT_Z,
-      startCartesian,
-      new Cesium.Cartesian3()
-    );
-    Cesium.Cartesian3.normalize(eastVector, eastVector);
-
-    const northVector = Cesium.Cartesian3.cross(
-      Cesium.Cartesian3.UNIT_Z,
-      eastVector,
-      new Cesium.Cartesian3()
-    );
-    Cesium.Cartesian3.normalize(northVector, northVector);
-
-    const bearing = Math.atan2(
-      Cesium.Cartesian3.dot(vector, eastVector),
-      Cesium.Cartesian3.dot(vector, northVector)
-    );
-    const bearingDegrees = Cesium.Math.toDegrees(bearing);
-
-    return (bearingDegrees + 360) % 360;
-  }
-
   viewer.entities.add({
     polyline: {
       positions: pointss,
@@ -190,15 +135,67 @@ const flight = async () => {
       material: new Cesium.PolylineArrowMaterialProperty(Cesium.Color.YELLOW),
     },
   });
+
+  const computeOrientation = () => {
+    const property = new Cesium.SampledProperty(Cesium.Quaternion);
+    let heading;
+    for (let i = 0; i < flightData.length - 1; i++) {
+      const time = Cesium.JulianDate.addSeconds(
+        start,
+        i * timeStepInSeconds,
+        new Cesium.JulianDate()
+      );
+
+      if (i === 0) {
+        const bearing = calculateCorrectBearing(
+          startCoordinate.latitude,
+          startCoordinate.longitude,
+          targetCoordinate.latitude,
+          targetCoordinate.longitude
+        );
+        // heading 계산
+        heading = Cesium.Math.toRadians(bearing - 90);
+        // 모델의 방향 설정
+        const hpr = new Cesium.HeadingPitchRoll(heading, 0.0, 0.0);
+        property.addSample(
+          time,
+          Cesium.Transforms.headingPitchRollQuaternion(startPosition, hpr)
+        );
+      } else {
+        const currentPosition = Cesium.Cartesian3.fromDegrees(
+          flightData[i].longitude,
+          flightData[i].latitude,
+          flightData[i].height
+        );
+
+        const bearing = calculateCorrectBearing(
+          flightData[i].latitude,
+          flightData[i].longitude,
+          flightData[i + 1].latitude,
+          flightData[i + 1].longitude
+        );
+        // heading 계산
+        heading = Cesium.Math.toRadians(bearing - 90);
+        // 모델의 방향 설정
+        const hpr = new Cesium.HeadingPitchRoll(heading, 0.0, 0.0);
+        property.addSample(
+          time,
+          Cesium.Transforms.headingPitchRollQuaternion(currentPosition, hpr)
+        );
+      }
+    }
+    return property;
+  };
+
   const positionProperty = computePositionProperty();
-  // const orientationProperty = computeOrientation();
+  const orientationProperty = computeOrientation();
   // 비행기객체 관련 데이터 추가
   const airplaneEntity = viewer.entities.add({
     availability: new Cesium.TimeIntervalCollection([
       new Cesium.TimeInterval({ start: start, stop: stop }),
     ]),
     position: positionProperty,
-    // orientation: orientationProperty,
+    orientation: orientationProperty,
 
     path: {
       resolution: 1,
@@ -216,30 +213,43 @@ const flight = async () => {
   });
   viewer.trackedEntity = airplaneEntity;
 
-  // 방향 벡터 계산
-  const direction = Cesium.Cartesian3.subtract(
-    targetPosition,
-    startPosition,
-    new Cesium.Cartesian3()
-  );
-  Cesium.Cartesian3.normalize(direction, direction);
-  const bearing = calculateBearingUsingCesium(
-    startCoordinate.latitude,
-    startCoordinate.longitude,
-    targetCoordinate.latitude,
-    targetCoordinate.longitude
-  );
+  //   viewer.scene.preRender.addEventListener(function () {
+  //     var currentTime = viewer.clock.currentTime;
+  //     var currentTimeString = Cesium.JulianDate.toIso8601(currentTime);
+  //     // JulianDate를 Date 객체로 변환합니다.
+  //     var currentDate = Cesium.JulianDate.toDate(currentTime);
 
-  // heading 계산
-  const heading2 = Cesium.Math.toRadians(bearing);
-  const heading = Math.atan2(direction.y, direction.x);
-  console.log(heading2, Cesium.Math.toDegrees(heading2));
-  // console.log(heading2, Cesium.Math.toDegrees(heading2));
-  // 모델의 방향 설정
-  const hpr = new Cesium.HeadingPitchRoll(heading2, 0.0, 0.0);
-  airplaneEntity.orientation = Cesium.Transforms.headingPitchRollQuaternion(
-    startPosition,
-    hpr
-  );
+  //     // Date 객체의 시간을 초 단위로 가져옵니다.
+  //     var currentTimeInSeconds = Math.floor(
+  //       currentDate.getTime() / 1000
+  //     ).toString();
+
+  //     if (
+  //       +currentTimeInSeconds.slice(
+  //         currentTimeInSeconds.length - 2,
+  //         currentTimeInSeconds.length
+  //       ) > timeStepInSeconds
+  //     ) {
+  //       const bearing = calculateCorrectBearing(
+  //         flightData[2].latitude,
+  //         flightData[2].longitude,
+  //         flightData[3].latitude,
+  //         flightData[3].longitude
+  //       );
+  //       const asdPosition = Cesium.Cartesian3.fromDegrees(
+  //         flightData[2].longitude,
+  //         flightData[2].latitude,
+  //         flightData[2].height
+  //       );
+  //       // heading 계산
+  //       const heading = Cesium.Math.toRadians(bearing - 90);
+  //       // 모델의 방향 설정
+  //       const hpr = new Cesium.HeadingPitchRoll(heading, 0.0, 0.0);
+  //       airplaneEntity.orientation = Cesium.Transforms.headingPitchRollQuaternion(
+  //         asdPosition,
+  //         hpr
+  //       );
+  //     }
+  //   });
 };
 flight();
